d5399ccc2b9c908e1a549aecc64c28556656aec1
Mapc0177 <mapc@dlmu.edu.cn>
2025-09-28 21:35:15 +0800

    Delete FORTIFY directory

--- FORTIFY/tests/test_keywords.py
+++
@@ -1,9 +0,0 @@
-from fortify_scg.keywords import compile_patterns, is_sensitive
-
-def test_keyword_matcher():
-    kw = ["strcpy", "*RC6*", "MD5_*"]
-    rgx = compile_patterns(kw)
-    assert is_sensitive("strcpy", rgx)
-    assert is_sensitive("fastRC6encrypt", rgx)
-    assert is_sensitive("MD5_Init", rgx)
-    assert not is_sensitive("safe_function", rgx)
